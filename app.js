2dee60c951d8c94ae9b8446d02ef92c741dfbc3f
Dhruv <dhruvj5418@gmail.com>
2020-06-13 13:46:26 -0400

    setting id for new itwm is done.

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var budgetController = (function (){
         this.description=description;
         this.value=value;
     };
-
     var data={
         allItems:{
             exp: [],
@@ -20,6 +19,32 @@ var budgetController = (function (){
              exp: 0,
              inc: 0
          }
+    };
+    return{
+        addItem: function(type,des,val){
+            var newItem,ID;
+            
+            //create new ID
+            if(data.allItems[type].length > 0){
+                ID=data.allItems[type][data.allItems[type].length-1].id+1;
+            }else{
+                ID=0;
+            }
+           
+
+            //create new item based on type
+            if(type==='exp'){
+                newItem = new Expense(ID,des,val);
+            }else if(type==='inc'){
+                newItem = new Income(ID,des,val);
+            }
+            //push it to data structure
+            data.allItems[type].push(newItem); 
+            return newItem;
+        },
+        testing:function(){
+            console.log(data);
+        }
     };
 })();
 
@@ -62,11 +87,12 @@ var controller = (function(budgetCtrl,UIctrl){
 
     };
     var ctrlAddItem = function(){
+        var newItem,input;
         //1. Get the field input data
-        var input = UIController.getInput();
+         input = UIController.getInput();
 
         //2.Add the item to the budget controller
-
+         newItem = budgetCtrl.addItem(input.type,input.description,input.value);
 
         //3. Add the item to the UI
 
